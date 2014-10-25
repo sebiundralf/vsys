db390e9edecf2ef97a2fc1b06259b1a1fb7fd2ca
Sebastian <if13b067@technikum-wien.at>
2014-10-25 20:05:22 +0200

    Beispielcode in Server und Client hinzugefügt --> verbindung kann hergestellt werden

--- client_main.c
+++ client_main.c
@@ -3,6 +3,12 @@
 #include <getopt.h>
 #include <errno.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#define BUF 1024
 
 void print_usage()
 {
@@ -20,59 +26,102 @@ int main(int argc, char* argv[])
 
     /* Start der GETOPT behandlung */
     {
-    int c;
-    int counter = 0;
-    int sl = 0;
+        int c;
+        int counter = 0;
+        int sl = 0;
 
-    if(argc <3)
-        print_usage();
-
-    while( (c = getopt(argc, argv, "i:p:")) != EOF )
-    {
-
-        switch (c)
-        {
-        case 'p':
-            if(vport!=-1)
-            {
-                print_usage();
-                break;
-            }
-            vport = strtol(optarg,(char**)NULL,10);
-            counter++;
-            break;
-
-        case 'i':
-            if(vip!=NULL)
-            {
-                print_usage();
-                break;
-            }
-            sl = strlen(optarg);
-            vip = (char*) malloc(sizeof(char)* (sl+1));
-            strcpy(vip, optarg);
-            counter++;
-            break;
-
-        case '?':
+        if(argc <3)
             print_usage();
-            break;
+
+        while( (c = getopt(argc, argv, "i:p:")) != EOF )
+        {
+
+            switch (c)
+            {
+            case 'p':
+                if(vport!=-1)
+                {
+                    print_usage();
+                    break;
+                }
+                vport = strtol(optarg,(char**)NULL,10);
+                counter++;
+                break;
+
+            case 'i':
+                if(vip!=NULL)
+                {
+                    print_usage();
+                    break;
+                }
+                sl = strlen(optarg);
+                vip = (char*) malloc(sizeof(char)* (sl+1));
+                strcpy(vip, optarg);
+                counter++;
+                break;
+
+            case '?':
+                print_usage();
+                break;
+            }
         }
+
+        // printf("\nIP: %s, Port: %d\n",vip, vport);
+
+        // printf("\nCounter: %d; optind: %d; argc: %d\n", counter, optind, argc);
+
+        if(counter!= 2 || optind!=argc || vport < 1)
+            print_usage();
+
+
+        //printf("\n FERTIG\n ");
+        /* ENDE GETOPT */
     }
 
-         // printf("\nIP: %s, Port: %d\n",vip, vport);
+    /* Verbindungsaufbau */
+    {
+        int create_socket;
+        char buffer[BUF];
+        struct sockaddr_in address;
+        int size;
 
-      // printf("\nCounter: %d; optind: %d; argc: %d\n", counter, optind, argc);
+        if ((create_socket = socket (AF_INET, SOCK_STREAM, 0)) == -1)
+        {
+            perror("Socket error");
+            return EXIT_FAILURE;
+        }
 
-    if(counter!= 2 || optind!=argc || vport < 1)
-        print_usage();
+        memset(&address,0,sizeof(address));
+        address.sin_family = AF_INET;
+        address.sin_port = htons (vport);
+        inet_aton (vip, &address.sin_addr);
 
+        if (connect ( create_socket, (struct sockaddr *) &address, sizeof (address)) == 0)
+        {
+            printf ("Connection with server (%s) established\n", inet_ntoa (address.sin_addr));
+            size=recv(create_socket,buffer,BUF-1, 0);
+            if (size>0)
+            {
+                buffer[size]= '\0';
+                printf("%s",buffer);
+            }
+        }
+        else
+        {
+            perror("Connect error - no server available");
+            return EXIT_FAILURE;
+        }
 
-    //printf("\n FERTIG\n ");
-    /* ENDE GETOPT */
+        do
+        {
+            printf ("Send message: ");
+            fgets (buffer, BUF, stdin);
+            send(create_socket, buffer, strlen (buffer), 0);
+        }
+        while (strcmp (buffer, "quit\n") != 0);
+        close (create_socket);
+
     }
-
-
-
+    /* Ende Verbindung */
     return 0;
 }
--- server_main.c
+++ server_main.c
@@ -3,6 +3,12 @@
 #include <getopt.h>
 #include <errno.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#define BUF 1024
 
 /* Funktion print_usage() zur Ausgabe der usage Meldung */
 void print_usage()
@@ -22,61 +28,121 @@ int main(int argc, char* argv[])
 
     /* Start der GETOPT behandlung */
     {
-    int c;
-    int counter = 0;
-    int sl = 0;
+        int c;
+        int counter = 0;
+        int sl = 0;
 
-    if(argc <3)
-        print_usage();
-
-    while( (c = getopt(argc, argv, "d:p:")) != EOF )
-    {
-
-        switch (c)
-        {
-        case 'p':
-            if(vport!=-1)
-            {
-                print_usage();
-                break;
-            }
-            vport = strtol(optarg,(char**)NULL,10);
-            counter++;
-            break;
-
-        case 'd':
-            if(vdir!=NULL)
-            {
-                print_usage();
-                break;
-            }
-            sl = strlen(optarg);
-            vdir = (char*) malloc(sizeof(char)* (sl+1));
-            strcpy(vdir, optarg);
-            counter++;
-            break;
-
-        case '?':
+        if(argc <3)
             print_usage();
-            break;
+
+        while( (c = getopt(argc, argv, "d:p:")) != EOF )
+        {
+
+            switch (c)
+            {
+            case 'p':
+                if(vport!=-1)
+                {
+                    print_usage();
+                    break;
+                }
+                vport = strtol(optarg,(char**)NULL,10);
+                counter++;
+                break;
+
+            case 'd':
+                if(vdir!=NULL)
+                {
+                    print_usage();
+                    break;
+                }
+                sl = strlen(optarg);
+                vdir = (char*) malloc(sizeof(char)* (sl+1));
+                strcpy(vdir, optarg);
+                counter++;
+                break;
+
+            case '?':
+                print_usage();
+                break;
+            }
         }
+
+
+        //  printf("\nPfad: %s, Port: %d\n",vdir, vport);
+
+        //    printf("\nCounter: %d; optind: %d; argc: %d\n", counter, optind, argc);
+
+        if(counter!= 2 || optind!=argc || vport < 1)
+            print_usage();
+        //printf("perfekt");
+
+
+        /* ENDE GETOPT*/
     }
 
 
-    //  printf("\nPfad: %s, Port: %d\n",vdir, vport);
+    /* ANFANG Verbindungsaufbau */
+    {
+        int create_socket, new_socket;
+        socklen_t addrlen;
+        char buffer[BUF];
+        int size;
+        struct sockaddr_in address, cliaddress;
 
-    //    printf("\nCounter: %d; optind: %d; argc: %d\n", counter, optind, argc);
+        create_socket = socket (AF_INET, SOCK_STREAM, 0);
 
-    if(counter!= 2 || optind!=argc || vport < 1)
-        print_usage();
-    //printf("perfekt");
+        memset(&address,0,sizeof(address));
+        address.sin_family = AF_INET;
+        address.sin_addr.s_addr = INADDR_ANY;
+        address.sin_port = htons (vport);
 
+        if (bind ( create_socket, (struct sockaddr *) &address, sizeof (address)) != 0)
+        {
+            perror("bind error");
+            return EXIT_FAILURE;
+        }
+        listen (create_socket, 5);
 
-    /* ENDE GETOPT*/
+        addrlen = sizeof (struct sockaddr_in);
+
+        while (1)
+        {
+            printf("Waiting for connections...\n");
+            new_socket = accept ( create_socket, (struct sockaddr *) &cliaddress, &addrlen );
+            if (new_socket > 0)
+            {
+                printf ("Client connected from %s:%d...\n", inet_ntoa (cliaddress.sin_addr),ntohs(cliaddress.sin_port));
+                strcpy(buffer,"Welcome to myserver, Please enter your command:\n");
+                send(new_socket, buffer, strlen(buffer),0);
+            }
+            do
+            {
+                size = recv (new_socket, buffer, BUF-1, 0);
+                if( size > 0)
+                {
+                    buffer[size] = '\0';
+                    printf ("Message received: %s\n", buffer);
+                }
+                else if (size == 0)
+                {
+                    printf("Client closed remote socket\n");
+                    break;
+                }
+                else
+                {
+                    perror("recv error");
+                    return EXIT_FAILURE;
+                }
+            }
+            while (strncmp (buffer, "quit", 4)  != 0);
+            close (new_socket);
+        }
+        close (create_socket);
+
     }
+    /* ENDE VERBINDUNG */
 
 
-
-
-return 0;
+    return 0;
 }
